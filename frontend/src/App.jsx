6694d84f0d367fd5d080d8ae1d86fe66cd51dd88
Sovan David <zenitsuidc@email.com>
2026-05-20 20:06:41 +0700

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Aquarium E-Commerce
+      </h1>
+
+      <p className="mt-4">
+        Frontend is running successfully.
+      </p>
+    </div>
+  )
+}
+
+export default App
